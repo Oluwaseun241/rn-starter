d457162ec5161733926f97b9807790360f3283f2
Oluwaseun241 <tanimolaoluwaseun70@gmail.com>
2024-11-09 13:11:12 +0100

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Native Starter 👋
+
+using `bun install`
+
+## Feature
+
+- [x] Expo router
+- [x] Nativewind
---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView, Text } from "react-native";
+
+export default function index() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center">
+      <Text className="text-white text-lg">Cooking...</Text>
+    </SafeAreaView>
+  );
+}
